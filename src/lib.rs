0385bc4b3caea357a1c2cccf4d851cf538de9ab4
Janitherange <jeerange@gmail.com>
2022-03-12 20:11:00 +0530

    Project Completed

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+use wasm_bindgen::prelude::*;
+use web_sys::console::log_1 as log;
+use base64::{encode, decode};
+use image::load_from_memory;
+use image::ImageOutputFormat::Png;
+
+#[wasm_bindgen]
+pub fn grayscale(encoded_file:&str)->String{
+    log(&"Grayscale called".into());
+
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"Image decoded".into());
+
+    let mut image = load_from_memory(&base64_to_vector).unwrap();
+    log(&"Image loaded".into());
+
+    image = image.grayscale();
+
+    log(&"Grayscal effect applied".into());
+
+    let mut buffer = vec![];
+
+    image.write_to(
+        &mut buffer,
+        Png
+    ).unwrap();
+
+    log(&"New image written".into());
+
+    let encode_img = encode(&buffer);
+    let data_url = format!("data:image/png;base64,{}", encode_img);
+
+    data_url
+}
